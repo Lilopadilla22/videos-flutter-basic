9178c809b368cb277aa367a379e59752f903c7ba
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-16 16:20:23 -0500

    add provider in main

---
+++ lib/presentation/providers/discover_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:videos_flutter_basic/domain/entities/video_post.dart';
+
+class DiscoverProvider  extends ChangeNotifier{
+
+  bool initialLoading = true;
+  List<VideoPost> videos = [];
+
+  Future<void> loadNextPage() async {
+
+
+    notifyListeners();
+  }
+}
