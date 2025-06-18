bae84bcb0c372507a76b06826ec183e38a604944
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-18 11:17:28 -0500

    update video scroll

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:videos_flutter_basic/domain/entities/video_post.dart';
 import 'package:videos_flutter_basic/presentation/widgets/shared/video_buttons.dart';
+import 'package:videos_flutter_basic/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
 
@@ -23,6 +24,12 @@ class VideoScrollableView extends StatelessWidget {
         return Stack(
           children: [
             //video player + gradiente
+            SizedBox.expand(
+              child: FullscreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              ),
+            ),
             // botones
             Positioned(
               bottom: 40,
