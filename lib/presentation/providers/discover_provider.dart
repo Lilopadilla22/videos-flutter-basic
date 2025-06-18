2ca513b657b4b63d740e12b8d58e69f09f4298ea
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-18 09:43:42 -0500

    update provider add videos

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:videos_flutter_basic/domain/entities/video_post.dart';
+import 'package:videos_flutter_basic/domain/shared/data/local_video_posts.dart';
+import 'package:videos_flutter_basic/infrastructure/models/local_video_model.dart';
 
 class DiscoverProvider  extends ChangeNotifier{
 
@@ -8,6 +10,15 @@ class DiscoverProvider  extends ChangeNotifier{
 
   Future<void> loadNextPage() async {
 
+    await Future.delayed(Duration(seconds: 2));
+
+    final List<VideoPost> newVideos = videoPosts.map(
+      (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()
+    ).toList();
+
+    videos.addAll(newVideos);
+    
+    initialLoading = false;
 
     notifyListeners();
   }
