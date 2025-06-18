ae3233e64228fab694c9da1ebf81dd64196bab5c
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-18 09:44:16 -0500

    add scroll view

---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:videos_flutter_basic/domain/entities/video_post.dart';
+import 'package:videos_flutter_basic/presentation/widgets/shared/video_buttons.dart';
+
+class VideoScrollableView extends StatelessWidget {
+
+  final List<VideoPost> videos;
+
+  const VideoScrollableView({
+    super.key, 
+    required this.videos
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      physics: BouncingScrollPhysics(),
+      itemCount: videos.length,
+      itemBuilder: (context, index){
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            //video player + gradiente
+            // botones
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost)
+            ),
+          ],
+        );
+      } ,
+    );
+  }
+}
+
