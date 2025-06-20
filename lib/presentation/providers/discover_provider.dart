006c36aca3e8727e935c38e1c2e47986335fb40c
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-20 13:03:13 -0500

    update repository and dataSource

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:videos_flutter_basic/domain/entities/video_post.dart';
+import 'package:videos_flutter_basic/domain/repositories/video_posts_repository.dart';
 import 'package:videos_flutter_basic/domain/shared/data/local_video_posts.dart';
 import 'package:videos_flutter_basic/infrastructure/models/local_video_model.dart';
 
-class DiscoverProvider  extends ChangeNotifier{
+class DiscoverProvider extends ChangeNotifier {
+  final VideoPostsRepository videoRepository;
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
-  Future<void> loadNextPage() async {
+  DiscoverProvider({required this.videoRepository});
 
+  Future<void> loadNextPage() async {
     await Future.delayed(Duration(seconds: 2));
 
-    final List<VideoPost> newVideos = videoPosts.map(
-      (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()
-    ).toList();
+    // final List<VideoPost> newVideos = videoPosts
+    //     .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+    //     .toList();
+
+    final newVideos = await videoRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
-    
+
     initialLoading = false;
 
     notifyListeners();
